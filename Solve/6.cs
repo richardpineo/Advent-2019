d40134bf1b7f8005cb8350d06405eb192cbcbda5
Richard Pineo <Richard.E.Pineo@ge.com>
2019-12-05 21:58:22 -0800

    6

--- Solve/6.cs
+++ Solve/6.cs
@@ -13,7 +13,7 @@ class Solve6 : ISolve
 
     const string Input = "Input//6.txt";
     const string ExampleA = "Examples//6a.txt";
-    const string ExampleB = "Examples//6a.txt";
+    const string ExampleB = "Examples//6b.txt";
 
     public bool Prove(bool isA)
     {
@@ -69,9 +69,40 @@ class Solve6 : ISolve
         return 1 + orbitees(orbitee, spaceoids);
     }
 
+    private void getPath(string name, Spaceoids s, ref List<string> path)
+    {
+        string orbitee;
+        if (!s.TryGetValue(name, out orbitee))
+        {
+            return;
+        }
+        path.Add(name);
+        getPath(orbitee, s, ref path);
+    }
+
+    private int countB(Spaceoids s)
+    {
+        var you = new List<string>();
+        getPath(s["YOU"], s, ref you);
+        var santa = new List<string>();
+        getPath(s["SAN"], s, ref santa);
+
+        for (var i = 0; i < you.Count; i++)
+        {
+            var santaIdx = santa.FindIndex(s => s == you[i]);
+            if (-1 != santaIdx)
+            {
+                return i + santaIdx;
+            }
+        }
+        return -1;
+    }
+
     private bool evaluateB(Spaceoids s)
     {
-        return false;
+        int count = countB(s);
+        int answer = 4;
+        return count == answer;
     }
 
     public string Solve(bool isA)
@@ -92,8 +123,7 @@ class Solve6 : ISolve
 
     private string SolveB(Spaceoids s)
     {
-
-        return "unknown";
+        int count = countB(s);
+        return count.ToString();
     }
-
 }

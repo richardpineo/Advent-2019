1c242f6bd1ee868eeebc4070da1ef55577c6083f
Richard Pineo <Richard.E.Pineo@ge.com>
2020-01-02 14:46:52 -0800

    24a

--- Solve/24.cs
+++ Solve/24.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
+using System.Linq;
 
 class Solve24 : ISolve
 {
@@ -11,10 +12,20 @@ class Solve24 : ISolve
     }
 
     const string Input = "Input//24.txt";
+    const string Example = "Examples//24.txt";
 
     public bool Prove(bool isA)
     {
-        return true;
+        var eris = new Eris();
+        var lines = File.ReadAllLines(Example, Encoding.UTF8);
+        for (var y = 0; y < 5; y++)
+        {
+            for (var x = 0; x < 5; x++)
+            {
+                eris.SetBug(x, y, lines[y][x] == '#');
+            }
+        }
+        return SolveFor(eris, false) == 2129920;
     }
 
     public string Solve(bool isA)
@@ -26,6 +37,10 @@ class Solve24 : ISolve
     {
         public bool GetBug(int x, int y)
         {
+            if (x < 0 || x >= 5 || y < 0 || y >= 5)
+            {
+                return false;
+            }
             return bugs[y * 5 + x];
         }
         public void SetBug(int x, int y, bool isBug)
@@ -38,11 +53,56 @@ class Solve24 : ISolve
             long hash = 0;
             for (var i = 0; i < 25; i++)
             {
-                hash += bugs[i] ? PowerOf2(i) : 0;
+                if (bugs[i])
+                {
+                    hash += PowerOf2(i);
+                }
             }
             return hash;
         }
-        public static long PowerOf2(long power)
+
+        public void Trace()
+        {
+            Console.WriteLine("");
+            for (var y = 0; y < 5; y++)
+            {
+                for (var x = 0; x < 5; x++)
+                {
+                    Console.Write(GetBug(x, y) ? "#" : ".");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine(Hash().ToString());
+        }
+
+        int NeighboringBugs(int x, int y)
+        {
+            var bugs = new bool[] {
+                GetBug(x-1, y),
+                GetBug(x+1, y),
+                GetBug(x, y-1),
+                GetBug(x, y+1),
+            };
+            return bugs.Sum(b => b ? 1 : 0);
+        }
+
+        public Eris Iterate()
+        {
+            var next = new Eris();
+            for (var y = 0; y < 5; y++)
+            {
+                for (var x = 0; x < 5; x++)
+                {
+                    int neighbors = NeighboringBugs(x, y);
+                    bool hasBug = GetBug(x, y);
+                    bool isBug = hasBug ? (neighbors == 1) : (neighbors == 1 || neighbors == 2);
+                    next.SetBug(x, y, isBug);
+                }
+            }
+            return next;
+        }
+
+        static long PowerOf2(long power)
         {
             long result = 1;
             for (long i = 0; i < power; i++)
@@ -64,8 +124,28 @@ class Solve24 : ISolve
                 eris.SetBug(x, y, lines[y][x] == '#');
             }
         }
+        return SolveFor(eris, false).ToString();
+    }
 
-        return "Not yet";
+    private long SolveFor(Eris eris, bool trace)
+    {
+        var patterns = new HashSet<long>();
+        var next = eris;
+        while (true)
+        {
+            var hash = next.Hash();
+            if (patterns.Contains(hash))
+            {
+                return hash;
+            }
+            patterns.Add(hash);
+            next = next.Iterate();
+
+            if (trace)
+            {
+                next.Trace();
+            }
+        }
     }
 
     private string SolveB()

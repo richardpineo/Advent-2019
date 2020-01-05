25f4175d5c9205d0dc5786feff15e9e1ee12e631
Richard Pineo <Richard.E.Pineo@ge.com>
2020-01-05 14:52:21 -0800

    25a

--- Solve/25.cs
+++ Solve/25.cs
@@ -17,6 +17,11 @@ class Solve25 : ISolve
 
     public bool Prove(bool isA)
     {
+        if (isA)
+        {
+            var answer = findTheCode();
+            // Console.WriteLine(fullTrace);
+        }
         return true;
     }
 
@@ -27,26 +32,81 @@ class Solve25 : ISolve
 
     public string SolveA()
     {
-        semiautomatic();
-        return "Manual";
+        return findTheCode();
     }
 
-    public void semiautomatic()
+    StringBuilder fullTrace = new StringBuilder();
+
+    private string executeCommands(Intcode.State state, string[] commands, bool assumePrompt)
+    {
+        var lastLine = new StringBuilder();
+        var commandOutput = new StringBuilder();
+
+        var commandIndex = 0;
+        var input = "";
+        if (assumePrompt)
+        {
+            commandIndex++;
+            input = commands[0] + "\n";
+        }
+
+        var index = 0;
+        while (Intcode.Step(state))
+        {
+            var output = state.PopOutput();
+            if (output.HasValue)
+            {
+                char c = (char)output.Value;
+                fullTrace.Append(c);
+                commandOutput.Append(c);
+                switch (c)
+                {
+                    case '\n':
+                        if (lastLine.ToString() == "Command?")
+                        {
+                            if (commandIndex < commands.Length)
+                            {
+                                input = commands[commandIndex++];
+                                fullTrace.Append($"> {input}\n");
+                            }
+                            else
+                            {
+                                // We are done
+                                return commandOutput.ToString();
+                            }
+                            input += '\n';
+                            index = 0;
+                        }
+                        lastLine.Clear();
+                        break;
+                    default:
+                        lastLine.Append(c);
+                        break;
+                }
+            }
+            if (!state.input.HasValue && index < input.Length)
+            {
+                state.input = input[index++];
+            }
+        }
+        return commandOutput.ToString();
+    }
+
+    private string findTheCode()
     {
         var lines = File.ReadAllLines(Input, Encoding.UTF8);
         var program = Intcode.ParseInput(lines[0]);
         var state = new Intcode.State(program);
 
-        var input = "";
         var fullTrace = new StringBuilder();
         var lastLine = new StringBuilder();
-        var lastResponse = "";
-        string[] commands = File.ReadAllLines(CommandFile, Encoding.UTF8);
+        string[] initializeCommands = File.ReadAllLines(CommandFile, Encoding.UTF8);
         string[] items = File.ReadAllLines(ItemFile, Encoding.UTF8);
-        var commandIndex = 0;
 
-        var dropAll = items.Select(i => "drop " + i);
-        var isCarrying = true;
+        var dropAll = items.Select(i => "drop " + i).ToArray();
+
+        // do the pre-canned commands
+        var initializeOutput = executeCommands(state, initializeCommands, false);
 
         // Assume the answer is somewhere between 2 and 6 tries.
         var combinations = new List<IEnumerable<string>>();
@@ -55,88 +115,34 @@ class Solve25 : ISolve
         combinations.AddRange(Util.GetKCombs(items, 4));
         combinations.AddRange(Util.GetKCombs(items, 5));
         combinations.AddRange(Util.GetKCombs(items, 6));
-        var combinationIndex = 0;
 
-        var isTryingItem = false;
-
-        var index = 0;
-        while (Intcode.Step(state))
+        foreach (var combo in combinations)
         {
-            var output = state.PopOutput();
-            if (output.HasValue)
-            {
-                char c = (char)output.Value;
-                Console.Write(c);
-                fullTrace.Append(c);
-                switch (c)
-                {
-                    case '\n':
-                        if (lastLine.ToString() == "Command?")
-                        {
-                            if (isTryingItem)
-                            {
-                                if (!lastResponse.ToString().Contains("Alert!"))
-                                {
-                                    Console.WriteLine("EUREKA");
-                                    Console.WriteLine(combinations[combinationIndex].Select(s => s).ToArray());
-                                    return;
-                                }
-                            }
-                            lastResponse = "";
+            // Drop all first
+            var dropAllOutput = executeCommands(state, dropAll, true);
 
-                            if (commandIndex < commands.Length)
-                            {
-                                input = commands[commandIndex++];
-                                Console.WriteLine("> " + input);
-                            }
-                            else
-                            {
-                                if (combinationIndex < combinations.Count)
-                                {
-                                    if (isCarrying)
-                                    {
-                                        isTryingItem = false;
+            // Generate commands for the items
+            var comboCommands = combo.Select(i => "take " + i).ToArray();
+            comboCommands = comboCommands.Append("east").ToArray();
+            var commandOutput = executeCommands(state, comboCommands, true);
 
-                                        // Drop everything.
-                                        commands = dropAll.ToArray();
-                                        commandIndex = 0;
-                                        isCarrying = false;
-                                        break;
-                                    }
-                                    else
-                                    {
-                                        isTryingItem = true;
-                                        // Now we are at the place with the stuff.
-                                        var tryItems = combinations[combinationIndex++];
-                                        // Generate commands for the items
-                                        commands = tryItems.Select(i => "take " + i).ToArray();
-                                        commands = commands.Append("east").ToArray();
-                                        commandIndex = 0;
-                                        break;
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Embarrassing failure");
-                                    return;
-                                }
-                                // input = Console.ReadLine();
-                            }
-                            input += '\n';
-                            index = 0;
-                        }
-                        lastResponse += "\n" + lastLine.ToString();
-                        lastLine.Clear();
-                        break;
-                    default:
-                        lastLine.Append(c);
-                        break;
-                }
-            }
-            if (!state.input.HasValue && index < input.Length)
+            if (!commandOutput.Contains("Alert!"))
             {
-                state.input = input[index++];
+                // Find the number:
+                var preceeding = "You should be able to get in by typing ";
+                var index = commandOutput.IndexOf(preceeding);
+                var startIndex = index + preceeding.Length;
+                var endIndex = commandOutput.IndexOf(" ", startIndex);
+                var answer = commandOutput.Substring(startIndex, endIndex - startIndex);
+                return answer.ToString(); // 319815680
             }
+        }
+
+        // Interactive:
+        while (true)
+        {
+            var input = Console.ReadLine();
+            executeCommands(state, new[] { input }, true);
         }
     }
     public string SolveB()
@@ -144,58 +150,3 @@ class Solve25 : ISolve
         return "Manual";
     }
 }
-
-
-
-/*
-
-    public void interactive()
-    {
-        var lines = File.ReadAllLines(Input, Encoding.UTF8);
-        var program = Intcode.ParseInput(lines[0]);
-        var state = new Intcode.State(program);
-
-        var input = "";
-        var fullTrace = new StringBuilder();
-        var lastLine = new StringBuilder();
-        var commands = new StringBuilder();
-
-        var index = 0;
-        while (Intcode.Step(state))
-        {
-            var output = state.PopOutput();
-            if (output.HasValue)
-            {
-                char c = (char)output.Value;
-                Console.Write(c);
-                fullTrace.Append(c);
-                switch (c)
-                {
-                    case '\n':
-                        if (lastLine.ToString() == "Command?")
-                        {
-                            // Dump previous commands
-                            Console.WriteLine(commands);
-
-                            // Waiting for input.
-                            input = Console.ReadLine();
-                            // Console.WriteLine("Input received: " + input);
-                            input += '\n';
-                            index = 0;
-                            commands.Append(input);
-                        }
-                        lastLine.Clear();
-                        break;
-                    default:
-                        lastLine.Append(c);
-                        break;
-                }
-            }
-            if (!state.input.HasValue && index < input.Length)
-            {
-                state.input = input[index++];
-            }
-        }
-    }
-
-*/
